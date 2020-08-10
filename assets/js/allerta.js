105ce058a2f2bce989df7c27ec01be69ff61094a
binc0 <42704408+binc0@users.noreply.github.com>
2020-08-10 14:02:14 +0200

    Update allerta.js

--- assets/js/allerta.js
+++ assets/js/allerta.js
@@ -2,16 +2,24 @@
  
      var allerta = document.getElementById("allerta");
      
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://pianiemergenza.protezionecivile.fvg.it/api/alerts.json?test?callback=?",
+  "method": "GET",
+  "headers": {
+    "Access-Control-Allow-Headers": "*",
+    "Content-Type": "application/json"
+  },
+}
 
 
-$.getJSON('https://pianiemergenza.protezionecivile.fvg.it/api/alerts.json?test=3?callback=?', function(data) {
-    // JSON result in `data` variable
-    console.log(data);
-               
-        if (data[2].level >= 0) {
+
+$.ajax(settings).done(function (jcontent){
+
+            //Popolazione superiore a 50000
+        if (jcontent[2].level >= 0) {
              allerta.innerHTML += '<div class="alert alert-danger" role="alert"> <h4 class="alert-heading">' + data[i].title + '</h4> <p>' + data[i].description + '</p>  <hr>  <a class="btn btn-primary" href="' + data[i].link_url + '" role="button">Allerta</a> <a class="btn btn-secondary" href="' + data[i].file_url + '" role="button">Bollettino</a> <button type="button" class="float-md-right btn btn-danger" data-dismiss="alert" aria-label="Close">  <span aria-hidden="true">Chiudi</span> </button> </div>';
         }
+    
 });
-
-
-
