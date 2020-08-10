b1118df7cfe5d9819093af18eab2dbecf319d7bb
binc0 <42704408+binc0@users.noreply.github.com>
2020-08-10 13:06:34 +0200

    Update allerta.js

--- assets/js/allerta.js
+++ assets/js/allerta.js
@@ -2,24 +2,13 @@
  
      var allerta = document.getElementById("allerta");
      
-var settings = {
-  "async": true,
-  "crossDomain": true,
-  "url": "https://pianiemergenza.protezionecivile.fvg.it/api/alerts.json?test",
-  "method": "GET",
-  "headers": {
-    "Access-Control-Allow-Headers": "*",
-    "Content-Type": "application/json"
-  },
-}
 
 
-
-$.ajax(settings).done(function (jcontent){
-
-            //Popolazione superiore a 50000
+$.getJSON('https://pianiemergenza.protezionecivile.fvg.it/api/alerts.json?test', function(jcontent) {
+    // JSON result in `data` variable
+    
+                //Popolazione superiore a 50000
         if (jcontent[2].level >= 0) {
              allerta.innerHTML += '<tr><th scope="row">' + i + '</th><td>' + jcontent[i].nome + '</td><td>SI</td><td>Popolazione superiore a 5000</td><td>' + jcontent[i].popolazione + '</td></tr>';
         }
-    
 });
