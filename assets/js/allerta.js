a9347e7ab055b875fade38b1d2babff820611822
binc0 <42704408+binc0@users.noreply.github.com>
2020-08-10 13:48:40 +0200

    Update allerta.js

--- assets/js/allerta.js
+++ assets/js/allerta.js
@@ -4,12 +4,12 @@
      
 
 
-$.getJSON('https://pianiemergenza.protezionecivile.fvg.it/api/alerts.json?test?callback=?', function(jcontent) {
+$.getJSON('https://pianiemergenza.protezionecivile.fvg.it/api/alerts.json?test=3?callback=?', function(data) {
     // JSON result in `data` variable
-    console.log(jcontent);
+    console.log(data);
                
-        if (jcontent[2].level >= 0) {
-             allerta.innerHTML += '<div class="alert alert-danger" role="alert"> <h4 class="alert-heading">' + jcontent[i].title + '</h4> <p>' + jcontent[i].description + '</p>  <hr>  <a class="btn btn-primary" href="' + jcontent[i].link_url + '" role="button">Allerta</a> <a class="btn btn-secondary" href="' + jcontent[i].file_url + '" role="button">Bollettino</a> <button type="button" class="float-md-right btn btn-danger" data-dismiss="alert" aria-label="Close">  <span aria-hidden="true">Chiudi</span> </button> </div>';
+        if (data[2].level >= 0) {
+             allerta.innerHTML += '<div class="alert alert-danger" role="alert"> <h4 class="alert-heading">' + data[i].title + '</h4> <p>' + data[i].description + '</p>  <hr>  <a class="btn btn-primary" href="' + data[i].link_url + '" role="button">Allerta</a> <a class="btn btn-secondary" href="' + data[i].file_url + '" role="button">Bollettino</a> <button type="button" class="float-md-right btn btn-danger" data-dismiss="alert" aria-label="Close">  <span aria-hidden="true">Chiudi</span> </button> </div>';
         }
 });
 
