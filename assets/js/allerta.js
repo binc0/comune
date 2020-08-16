712a1e339461d3b6cabe6a29a2703647b21a7837
binc0 <42704408+binc0@users.noreply.github.com>
2020-08-17 00:41:30 +0200

    Update allerta.js

--- assets/js/allerta.js
+++ assets/js/allerta.js
@@ -1,6 +1,6 @@
 var allerta = document.getElementById("allerta");
 
-$.getJSON('https://binco.me/test', function(data) {
+$.getJSON('https://binc0.github.io/comune/allerte.php', function(data) {
     console.alert(data)
     if (data[2].level >= 0) {
              allerta.innerHTML += '<div class="alert alert-danger" role="alert"> <h4 class="alert-heading">' + data[2].title + '</h4> <p>' + data[2].description + '</p>  <hr>  <a class="btn btn-primary" href="' + data[i].link_url + '" role="button">Allerta</a> <a class="btn btn-secondary" href="' + data[2].file_url + '" role="button">Bollettino</a> <button type="button" class="float-md-right btn btn-danger" data-dismiss="alert" aria-label="Close">  <span aria-hidden="true">Chiudi</span> </button> </div>';
