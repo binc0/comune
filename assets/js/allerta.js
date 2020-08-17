a4ff3c721ea1f0b2307e8dea28f484e02411ccf1
binc0 <42704408+binc0@users.noreply.github.com>
2020-08-17 02:53:52 +0200

    Update allerta.js

--- assets/js/allerta.js
+++ assets/js/allerta.js
@@ -11,4 +11,8 @@ $.ajax(settings).done(function (data) {
     if (data[0].level >= 0) {
              allerta.innerHTML += '<div class="alert alert-danger" role="alert"> <h4 class="alert-heading">' + data[0].title + '</h4> <p>' + data[0].description + '</p>  <hr>  <a class="btn btn-primary" href="' + data[0].link_url + '" target="_blank" role="button">Allerta</a> <a class="btn btn-secondary" href="' + data[0].file_url + '" target="_blank" role="button">Bollettino</a> <button type="button" class="float-md-right btn btn-danger" data-dismiss="alert" aria-label="Close">  <span aria-hidden="true">Chiudi</span> </button> </div>';
         }
+    
+    function closecookie(){
+        document.cookie = "closecookie=true; expires=' + data[0].dt_end + '; path=/";
+    }
 });
