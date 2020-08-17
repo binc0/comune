29f148fbbc09ed67d7c91f06f48fbc59d63368f8
binc0 <42704408+binc0@users.noreply.github.com>
2020-08-17 03:22:31 +0200

    Update allerta.js

--- assets/js/allerta.js
+++ assets/js/allerta.js
@@ -8,14 +8,14 @@ var settings = {
 
 $.ajax(settings).done(function (data) {
   console.log(data);
-    if (data[0].level >= 0) {
-             allerta.innerHTML += '<div class="alert alert-danger" role="alert"> <h4 class="alert-heading">' + data[0].title + '</h4> <p>' + data[0].description + '</p>  <hr>  <a class="btn btn-primary" href="' + data[0].link_url + '" target="_blank" role="button">Allerta</a> <a class="btn btn-secondary" href="' + data[0].file_url + '" target="_blank" role="button">Bollettino</a> <button type="button" class="float-md-right btn btn-danger" onclick="closecookie()" data-dismiss="alert" aria-label="Close">  <span aria-hidden="true">Chiudi</span> </button> </div>';
-        }
-    
+    if (getCookie("username") == true) {}
+    else if (data[0].level >= 0) {
+            allerta.innerHTML += '<div class="alert alert-danger" role="alert"> <h4 class="alert-heading">' + data[0].title + '</h4> <p>' + data[0].description + '</p>  <hr>  <a class="btn btn-primary" href="' + data[0].link_url + '" target="_blank" role="button">Allerta</a> <a class="btn btn-secondary" href="' + data[0].file_url + '" target="_blank" role="button">Bollettino</a> <button type="button" class="float-md-right btn btn-danger" onclick="closecookie()" data-dismiss="alert" aria-label="Close">  <span aria-hidden="true">Chiudi</span> </button> </div>';
+            }
 
 });
 
-    function closecookie(){
+function closecookie(){
         $.ajax(settings).done(function (data) {
             console.log(data[0].dt_end);
             document.cookie = "closecookie=true; expires=' + data[0].dt_end + '; path=/";
