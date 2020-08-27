e83522d645dfa3b94d99392c72d796aa0ff97669
binc0 <42704408+binc0@users.noreply.github.com>
2020-08-27 19:12:03 +0200

    Update allerta.js

--- assets/js/allerta.js
+++ assets/js/allerta.js
@@ -33,7 +33,7 @@ function closecookie(){
             var now = new Date();
                var minutes = 30;
                now.setTime(now.getTime() + (minutes * 60 * 1000));
-            console.log(now.toUTCString());
+            console.log(now.toUTCString()); 
             console.log(data[0].dt_end);
             data_fine = data[0].dt_end;
             document.cookie += 'allerta=true; expires=' + data_fine + '; path=/';
