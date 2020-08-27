d7d0f4b58a1168e81416a1255857bb6c72fe44b9
binc0 <42704408+binc0@users.noreply.github.com>
2020-08-27 18:44:53 +0200

    Update allerta.js

--- assets/js/allerta.js
+++ assets/js/allerta.js
@@ -31,7 +31,7 @@ var settings = {
 
 function closecookie(){
         $.ajax(settings).done(function (data) {
+            console.log(date.toUTCString(data[0].dt_end));
             document.cookie += 'closecookie=true; expires=' + data[0].dt_end + '; path=/';
-            console.log(document.cookie);
     });
     }
